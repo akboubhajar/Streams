4f5d867476e1ed2c6b1be0d10e702746761361cc
akboubhajar <hajarakboub@gmail.com>
2024-02-27 18:25:30 +0100

    v2

---
+++ src/main/java/emplyee/Employee.java
@@ -0,0 +1,35 @@
+package net.hajar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+class Employee {
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee(String name, String department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
---
+++ src/main/java/emplyee/Main.java
@@ -0,0 +1,2 @@
+package emplyee;public class Main {
+}
